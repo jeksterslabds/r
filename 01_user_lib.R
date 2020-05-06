c2f1a3f34d8b4b592b7a035173ef1e8e03e0fbce
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-05-07 02:23:07 +0800

    Setup user lib

---
+++ 01_user_lib.R
@@ -0,0 +1,7 @@
+source("https://raw.githubusercontent.com/jeksterslabds/jeksterslabRutils/master/R/util_txt2file.R")
+source("https://raw.githubusercontent.com/jeksterslabds/jeksterslabRterm/master/R/term_user_lib.R")
+term_user_lib(
+  libpath = NULL,
+  dir = Sys.getenv("HOME"),
+  overwrite = TRUE
+)
